c4ed865b6a69497d5dbcca8f592ecff56829d28d
laoyi <yibo_grace@163.com>
2016-09-28 15:18:53 +0800

    add hybrid smoke

--- tempest/api/compute/servers/test_server_actions.py
+++ tempest/api/compute/servers/test_server_actions.py
@@ -130,7 +130,7 @@ class ServerActionsTestJSON(base.BaseV2ComputeTest):
             self.assertTrue(new_boot_time > boot_time,
                             '%s > %s' % (new_boot_time, boot_time))
 
-    @test.attr(type='smoke')
+    @test.attr(type=['smoke', 'hybrid-smoke'])
     @test.idempotent_id('2cb1baf6-ac8d-4429-bf0d-ba8a0ba53e32')
     def test_reboot_server_hard(self):
         # The server should be power cycled
